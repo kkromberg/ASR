924779b71cf63a32dedee71feee045359cdc624a
isklyar <ilya.sklyar@rwth-aachen.de>
2016-11-26 20:46:21 +0100

    some changes in code

--- exercise/5/ApproximateSolution.py
+++ exercise/5/ApproximateSolution.py
@@ -1,5 +1,5 @@
 import sys
-class StartStopDetection:
+class SpeechDetection:
     def __init__(self, fileLocation="data/probe1.ascii"):
         self.data = []
         for line in open(fileLocation, 'r'):
@@ -11,24 +11,21 @@ class StartStopDetection:
         score -= self.multiply(temp,temp) / (segmentEnd - segmentBegin + 1)
         return score
 
-    def calculateMeanOfSpeech(self,sumCosts,segmentBegin,segmentEnd):
-        return sumCosts[segmentEnd] - sumCosts[segmentBegin - 1]/ (segmentEnd - segmentBegin + 1)
-
     def approximateSolution(self):
         self.numberOfCalculations = 0
         T = len(self.data)
         # Precompute values
-        dataSumTable = [0] * T
-        dataSquareSumTable = [0] * T
-        currentDataSum = 0
-        currentSquareDataSum = 0
-        for i in range(0, len(dataSquareSumTable)):
-            currentDataSum += self.data[i]
-            currentSquareDataSum += self.multiply(self.data[i], self.data[i])
-            dataSumTable[i] = currentDataSum
-            dataSquareSumTable[i] = currentSquareDataSum
+        sumCosts = [0] * T
+        squareSumCosts = [0] * T
+        currentSum = 0
+        currentSquareSum = 0
+        for i in range(0, len(squareSumCosts)):
+            currentSum += self.data[i]
+            currentSquareSum += self.multiply(self.data[i], self.data[i])
+            sumCosts[i] = currentSum
+            squareSumCosts[i] = currentSquareSum
 
-        #uniformboundaryinitialization
+        #uniform boundary initialization
         B1 = T / 2 - 1
         B2 = T / 2
         numberOfIterations = 3
@@ -37,19 +34,18 @@ class StartStopDetection:
             bestCosts = 123421134
             newBoundary = B1
             for t in range(1, B2-1):
-                newCosts = self.calculateScoreOfSegment(dataSumTable, dataSquareSumTable, 1, t) + self.calculateScoreOfSegment(dataSumTable, dataSquareSumTable, t+1, B2);
+                newCosts = self.calculateScoreOfSegment(sumCosts, squareSumCosts, 1, t) + self.calculateScoreOfSegment(sumCosts, squareSumCosts, t+1, B2);
 
                 if (newCosts < bestCosts):
                     newBoundary = t
                     bestCosts   = newCosts
-
 
             B1 = newBoundary
             #Optimize secondary boundary
             bestCosts = 123421134
             newBoundary = B2
             for t in range(B1+1, T-1):
-                newCosts = self.calculateScoreOfSegment(dataSumTable, dataSquareSumTable, B1+1, t) + self.calculateScoreOfSegment(dataSumTable, dataSquareSumTable, t+1, T-1);
+                newCosts = self.calculateScoreOfSegment(sumCosts, squareSumCosts, B1+1, t) + self.calculateScoreOfSegment(sumCosts, squareSumCosts, t+1, T-1);
 
                 if (newCosts < bestCosts):
                     newBoundary = t
@@ -58,8 +54,8 @@ class StartStopDetection:
         print "numberOfCalculations", self.numberOfCalculations
         print "boundaries:"
         print B1, B2
-        print "Mean of the speech: ", (dataSumTable[B2] - dataSumTable[B1]) / (B2 - B1)
-        print "Mean of two silence segments: ", (dataSumTable[B1] - dataSumTable[0]) / (B1+1), (dataSumTable[T - 1] - dataSumTable[B2]) / (T - B2)
+        print "Mean of the speech: ", (sumCosts[B2] - sumCosts[B1]) / (B2 - B1)
+        print "Mean of two silence segments: ", (sumCosts[B1] - sumCosts[0]) / (B1+1), (sumCosts[T - 1] - sumCosts[B2]) / (T - B2)
     def multiply(self, x, y):
         self.numberOfCalculations += 1
         return x * y
@@ -68,5 +64,5 @@ if __name__ == "__main__":
     fileLocation = "data/probe1.ascii"
     if (len(sys.argv) == 2):
         fileLocation = sys.argv[1]
-    s = StartStopDetection(fileLocation)
+    s = SpeechDetection(fileLocation)
     s.approximateSolution()
