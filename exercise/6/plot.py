b3cd9467947a9d5712b84d32fd5b6eaa40737ae3
Konstantin <konerg@desktop.konerg.de>
2016-12-04 16:50:35 +0000

    plot for speech boundaries

---
+++ exercise/6/plot.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+boundaries_exercise5 = [22, 116]
+boundaries_exercise6 = [22, 118]
+y = np.fromfile('data/probe1.ascii', sep='\n')
+for energie in y:
+    print energie
+x = np.array(xrange(0, len(y)))
+
+plt.plot(x, y, label="Energies")
+boundaries_plt = plt.vlines(boundaries_exercise5, np.min(y), np.max(y), colors='r', label="Speech boundaries exercise 5")
+boundaries_plt = plt.vlines(boundaries_exercise6, np.min(y), np.max(y), colors='g', label="Speech boundaries exercise 6")
+plt.xlabel('Time')
+plt.ylabel('Energy')
+plt.legend()
+
+plt.grid(True)
+plt.savefig("test.png")
+plt.show()
+
+'''
+energies = plt.plot(x, y, label="Energies")
+boundaries_plt = plt.vlines(boundaries, np.min(y), np.max(y), colors='r', label="Speech boundaries")
+plt.xlabel('time frame')
+plt.ylabel('log(energy)')
+plt.legend()
+
+plt.grid(True)
+plt.savefig("test.png")
+plt.show()
+'''
