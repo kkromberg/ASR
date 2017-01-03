e6c40c9d6d95ef8aa826d83c449719917ff2dac4
Konstantin <konerg@desktop.konerg.de>
2017-01-03 18:11:25 +0000

    plot and counting for b

--- exercise/9/translateWordAlignmentToPhonemeAlignment.py
+++ exercise/9/translateWordAlignmentToPhonemeAlignment.py
@@ -1,6 +1,72 @@
 import numpy as np
+import json
+import operator
+import matplotlib.pyplot as plt
 
 
+# plotting for b and c
+def plot(num_oberservations):
+    separated_tuples = zip(*num_oberservations)
+    print separated_tuples
+    plt.scatter(*separated_tuples)
+    plt.title('Number of observation for each mixture')
+    plt.xlabel('Mixture index')
+    plt.ylabel('Amount of observations')
+    plt.xticks(separated_tuples[0])
+    plt.show()
+
+# task b
+def num_obersvations_word(src_file):
+    word_alignment = open(src_file, 'r')
+    all_prefixes = [1, 13, 22, 28, 37, 46, 55, 67, 82, 88, 97]
+    num_observations = {-1: 0, 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0,
+                        7: 0, 8: 0, 9: 0, 10: 0}
+
+    for line in word_alignment:
+        line_int = np.fromstring(line, dtype=int, sep=' ')
+        current_prefix = line_int[0]
+        for elem in line_int:
+            if elem in all_prefixes and current_prefix != elem:
+                current_prefix = elem
+                if elem == 1:
+                    num_observations[0] += 1
+                elif elem == 13:
+                    num_observations[1] += 1
+                elif elem == 22:
+                    num_observations[2] += 1
+                elif elem == 28:
+                    num_observations[3] += 1
+                elif elem == 37:
+                    num_observations[4] += 1
+                elif elem == 46:
+                    num_observations[5] += 1
+                elif elem == 55:
+                    num_observations[6] += 1
+                elif elem == 67:
+                    num_observations[7] += 1
+                elif elem == 82:
+                    num_observations[8] += 1
+                elif elem == 88:
+                    num_observations[9] += 1
+                elif elem == 97:
+                    num_observations[10] += 1
+            # count each silence state as word
+            elif elem == 0:
+                num_observations[-1] += 1
+    # close file
+    word_alignment.close()
+    # write count to file
+    num_observations_sorted = sorted(num_observations.items(), key=lambda x: x[1], reverse=True)
+    count = open('num_obersvations_per_mixture.word', 'w')
+    for elem in num_observations_sorted:
+        count.write(str(elem[0]) + ' ' + str(elem[1]) + '\n')
+
+    count.close()
+    # plotting
+    plot(num_observations_sorted)
+
+
+num_obersvations_word('alignment.word')
 # task c
 def translate_word_to_phoneme(word):
     """
@@ -233,6 +299,6 @@ def word_to_phoneme(src_file, target_file):
     # close files
     word_alignment.close()
     phoneme_alignment.close()
-    
+
 # task c
 word_to_phoneme('alignment.word', 'alignment.phoneme')
